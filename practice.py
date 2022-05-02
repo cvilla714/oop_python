ba327413252dcd6b323f2a7664b9115e6f705603
Cosmel Villalobos <cosmelvg@gmail.com>
2022-05-02 17:16:53 -0400

    Uploaded practice
    Include new way to apply property

--- practice.py
+++ practice.py
@@ -2,19 +2,30 @@ class Person:
     def __init__(self, name):
         self.name = name
         self._salary = 0
-
-    def set_salary(self, salary):
-        if  salary < 0:
-            raise ValueError("Salary cannot be negative")
-        self._salary = salary
-
-    def get_salary(self):
+# using new way to add property
+    @property
+    def salary(self):
         return round(self._salary)
 
-    salary = property(get_salary, set_salary)
+    @salary.setter
+    def salary(self, value):
+        if value < 0:
+            raise ValueError('Invalid salary')
+        self._salary = value
+
+    # def set_salary(self, salary):
+    #     if  salary < 0:
+    #         raise ValueError("Salary cannot be negative")
+    #     self._salary = salary
+    #
+    # def get_salary(self):
+    #     return round(self._salary)
+
+    # salary = property(get_salary, set_salary)
 
 p = Person('Bob')
-p.salary = 103.50
+# p.salary = 103.7750
+p.salary = -103.7750
 print(p.salary)
 # p.set_salary(103)
 # print(p.set_salary)
